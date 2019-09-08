160172ddabd00c276d308a0eedb9a9f5502ceeef
ShristiGup <guptashristi25@gmail.com>
2019-09-08 20:30:10 +0530

    adding buttons and modal

--- burger-builder/src/components/Burger/Burger.js
+++ burger-builder/src/components/Burger/Burger.js
@@ -2,12 +2,20 @@ import React from 'react';
 import classes from './Burger.css';
 import BurgerIngredient from './BurgerIngredients/BurgerIngredients';
 const burger = (props) => {
-const transformedIngredients = Object.keys(props.ingredients)
+let transformedIngredients = Object.keys(props.ingredients)
 .map(igKey => {
     return [...Array(props.ingredients[igKey])].map((_,i) => {
      return  <BurgerIngredient key={igKey+1} type={igKey}/>
     });
-});
+})
+
+.reduce((arr,el) =>{
+    return arr.concat(el)
+},[]);
+
+if(transformedIngredients.length === 0){
+    transformedIngredients = <p>Plz add ingredients</p>
+}
 
     return(
 <div className={classes.Burger}>
